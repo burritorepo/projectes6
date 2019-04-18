fea94ca41cce25e1dabe019639eb27b9f07050e3
APPLE <apple@Mac-mini-de-APPLE.local>
2019-04-17 19:44:19 -0500

    it added pagination

--- src/scripts/pages/users.js
+++ src/scripts/pages/users.js
@@ -2,29 +2,47 @@
 import "../../styles/pages/_users.scss";
 
 class Users {
+    /**
+     * 
+     */
     constructor() {
-        console.log('contructor!!!')
         this.urlBase = 'https://reqres.in/api';
         this.init();
-    }
+    };
 
+    /**
+     * 
+     * @param {*} url 
+     * @param {*} query 
+     */
     getData(url, query) {
         return fetch(`${this.urlBase}/${url}${query}`)
     };
 
     async handleData(promise) {
+        try {
+            let response = await promise;
+            let users = await response.json();
 
-        let response = await promise;
-        let users = await response.json();
-        this.insertDom(this.makeTemplate(users), this.makePagination(users));
-
+            this.usersMakeTemplate(users);
+            this.paginationMakeTemplate(users);
+        } catch(err) {
+            this.handleError('err', err)
+        }
     };
 
+    /**
+     * 
+     */
     handleError() {
         console.log('error!!')
     };
 
-    makeTemplate(users) {
+    /**
+     * 
+     * @param {*} users 
+     */
+    usersMakeTemplate(users) {
         let { data: listUsers } = users;
         let usersArr = [];
         usersArr = listUsers.map((user) => {
@@ -39,43 +57,82 @@ class Users {
 
             return template;
         })
-        return `<div class="page page-users"><div class="container d-flex flex-nowrap">${usersArr.join('')}</div></div>`;
-    }
 
-    makePagination(users) {
-        let { total_pages } = users;
-        let colArr = [];
-        for(let i= 1; i<=total_pages; i++) {
-            colArr.push(`<li class="page-item">
-            <a class="page-link" href="users?page=${i}">${i}</a>
-          </li>`)
-        };
-        
-        let template = `<ul class="pagination justify-content-center">
-                ${colArr.join('')}   
-            </li>
-        </ul>`;
+        document.querySelector('#users').innerHTML = `<div class="page page-users"><div class="container d-flex flex-nowrap">${usersArr.join('')}</div></div>`;
+    };
 
+    /**
+     * 
+     * @param {*} users 
+     */
+    paginationMakeTemplate(users) {
 
-        return template;
-    }
+        if (!document.querySelector('#users-nav').children.length) {
+            let { total_pages, page } = users;
+            let ul = document.createElement('ul');
+            
+            ul.classList.add('pagination', 'justify-content-center');
 
-    insertDom(...element) {
-        document.querySelector('#main').innerHTML += element.join('');
+            let pagination = (query, e) => {
+                e.stopPropagation();
+                let liList = e.target.closest('ul').childNodes;
+                let currentLi = e.target.closest('li');
 
-        document.querySelectorAll('.page-link').forEach((element) => {
-            element.addEventListener('click', (e) => {
-                // console.log('click', e.target.href)
-                e.preventDefault();
-                // console.log(e.getAttribute("href"))
-                this.handleData(this.getData('users', '?page=3'));
-            })
-        })
-    }
+                liList.forEach(element => {
+                    element.classList.remove('active');
+                });
 
+                currentLi.classList.add('active');
+                
+                this.handleData(this.getData('users', query));
+            }
+
+            for (let i = 1; i <= total_pages; i++) {
+                let li = document.createElement('li');
+                let span = document.createElement('span');
+
+                span.classList.add('page-link');
+                span.innerHTML = `${i}`;
+                span.addEventListener('click', pagination.bind(this, `?page=${i}`));
+
+                li.appendChild(span);
+                li.classList.add('page-item');
+
+                if (i === page) {
+                    li.classList.add('active');
+                }
+
+                ul.appendChild(li);
+            };
+
+            document.querySelector('#users-nav').appendChild(ul);
+        }
+
+        return false;
+    };
+
+    /**
+     * 
+     */
+    makeTemplate() {
+        let wrapper = document.querySelector('#main');
+        let divContent = document.createElement('div');
+        let divNav = document.createElement('div');
+
+        divContent.setAttribute('id', 'users');
+        divNav.setAttribute('id', 'users-nav');
+
+        wrapper.appendChild(divContent);
+        wrapper.appendChild(divNav);
+    };
+
+    /**
+     * 
+     */
     init() {
-        this.handleData(this.getData('users', '?page=2'));
-    }
+        this.makeTemplate()
+        this.handleData(this.getData('users', '?page=1'));
+    };
 }
 
 new Users();
